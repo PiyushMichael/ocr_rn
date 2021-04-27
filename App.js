d9000c3d1509c202c99fe5c99737857c8b9acbeb
Piyush-021 <piyush@o2onelabs.com>
2021-04-28 02:30:37 +0530

    COMPLETED functionality.

--- App.js
+++ App.js
@@ -17,8 +17,13 @@ import {
   Dimensions,
   Modal,
   ScrollView,
+  ToastAndroid,
+  Share,
 } from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
 import {RNCamera} from 'react-native-camera';
+import Icon from 'react-native-vector-icons/Entypo';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -27,14 +32,26 @@ const {width} = Dimensions.get('window');
 const offset = (width * 1.5) / 16;
 
 const App: () => Node = () => {
-  const [textBlocks, setTextBlocks] = useState([]);
+  const [blocks, seteBlocks] = useState([]);
   const [modal, setModal] = useState(false);
 
   let text = '';
-  textBlocks.forEach((block, i) => {
+  blocks.forEach((block, i) => {
     text += block.value + '\n';
   });
-  console.log(text);
+
+  const copy = () => {
+    Clipboard.setString(text);
+    ToastAndroid.show(
+      'Text copied to Clipboard',
+      ToastAndroid.SHORT,
+      ToastAndroid.TOP,
+    );
+  };
+
+  const share = () => {
+    Share.share({message: text});
+  };
 
   return (
     <View style={styles.container}>
@@ -56,33 +73,55 @@ const App: () => Node = () => {
         //   console.log(barcodes);
         // }}
         // onFacesDetected={faces => console.log(faces)}
-        onTextRecognized={({textBlocks}) => setTextBlocks(textBlocks)}
+        onTextRecognized={({textBlocks}) => {
+          if (!modal) {
+            seteBlocks(textBlocks);
+          }
+        }}
       />
       <View style={styles.buttonRow}>
         <TouchableOpacity onPress={() => setModal(true)} style={styles.capture}>
-          <Text style={styles.scanText}> SCAN</Text>
+          <Text style={styles.scanText}>VIEW</Text>
         </TouchableOpacity>
       </View>
-      {textBlocks.map((block, index) => (
-        <View
-          key={index.toString()}
-          style={{
-            position: 'absolute',
-            left: block.bounds.origin.x + offset,
-            top: block.bounds.origin.y,
-            width: block.bounds.size.width,
-            height: block.bounds.size.height,
-            borderWidth: 2,
-            borderColor:  'red',
-          }}
-        />
+      {!modal && blocks.map((block, index) => (
+          <View
+            key={index.toString()}
+            style={{
+              position: 'absolute',
+              left: block.bounds.origin.x + offset,
+              top: block.bounds.origin.y,
+              width: block.bounds.size.width,
+              height: block.bounds.size.height,
+              borderWidth: 2,
+              borderColor:  'red',
+            }}
+          />
       ))}
       <Modal visible={modal} onRequestClose={() => setModal(false)} transparent>
         <View style={styles.backdrop}>
           <View style={styles.modalWindow}>
+            <View style={styles.endROw}>
+              <Icon
+                name="circle-with-cross"
+                color={Colors.dark}
+                size={32}
+                onPress={() => setModal(false)}
+              />
+            </View>
             <ScrollView>
-              <Text>{text}</Text>
+              <Text style={styles.previewText}>{text}</Text>
             </ScrollView>
+            <View style={styles.bottomRow}>
+              <TouchableOpacity style={styles.button} onPress={share}>
+                <Icon name="share" size={24} />
+                <Text style={styles.buttonLabel}>Share</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={copy}>
+                <FeatherIcon name="copy" size={26} />
+                <Text style={styles.buttonLabel}>Copy</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Modal>
@@ -124,13 +163,41 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.4)',
+    backgroundColor: 'rgba(0,0,0,0.7)',
   },
   modalWindow: {
-    height: '70%',
+    flex: 1,
+    marginVertical: '20%',
     width: '90%',
     borderRadius: 8,
     backgroundColor: '#fff',
+  },
+  previewText: {
+    flexWrap: 'wrap',
+    margin: 10,
+    color: '#1292B4',
+  },
+  endROw: {
+    margin: 8,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  bottomRow: {
+    margin: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: '#ffc107',
+  },
+  buttonLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginHorizontal: 8,
   },
 });
 
